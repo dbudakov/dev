d90f97c51efdd1a0201718dbc02a466e56b73031
Dmitriy Budakov <budakov.web@gmail.com>
2021-08-17 18:22:36 +0300

    next

--- column.custom/old/ft_column1.c
+++ column.custom/old/ft_column1.c
@@ -1,5 +1,5 @@
 /*budakvoda, https://github.com/dbudakov */
-#include <string.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,29 +8,30 @@
 #define SYMBOL ' '
 #define NEGATIVE 1
 
-// #define FILE "./testfile"
-// #define NUM_COLUMN 3
+#define FILE "./testfile"
+#define NUM_COLUMN 1
 
 size_t get_len_file(int fd)
 {
 	size_t num;
 
-	// printf("%d", fd);
 	lseek(fd, 0, SEEK_END);
 	num = lseek(fd,0, SEEK_CUR);
 	lseek(fd, 0, SEEK_SET);
+	printf("len = %d\n", num);
 	return num;
 }
 
 char *file_to_str(int fd, size_t len)
 {
+	char 	str1[len];
 	char	*str;
 
-	// printf("%d ", len);
-	str = (char *)malloc(len + 1);
-	read(fd, str, len);
-	str[len] = '\0';
-	// printf("%s", str);
+	// str = (char *)malloc(len + 1);
+	read(fd, &str1, len);
+	str1[len] = '\0';
+	str = str1;
+
 	return str;
 }
 
@@ -86,10 +87,11 @@ int **get_arr_pre(char *str_file, int num_str, int num_max_word)
 	int **arr_pre;
 
 	i = 0;
-	arr_pre = (int**)malloc(num_str * sizeof(int *));
+	arr_pre = (int**)malloc(num_str);
 	while(i < num_str)
 	{
-		arr_pre[i] = (int *)malloc(num_max_word * sizeof(int));
+		arr_pre[i] = (int*)malloc(1);
+		// printf("%d, ", sizeof(arr_pre[2][1]));
 		i++;
 	}
 
@@ -101,6 +103,7 @@ int **get_arr_pre(char *str_file, int num_str, int num_max_word)
 		if (str_file[i] == SYMBOL || str_file[i] == '\n')
 		{
 			arr_pre[j][k++] = s;
+			// printf("%d,%d: %d\n", j,k, arr_pre[j][k-1]);
 			s = 0;
 			if (str_file[i] != '\n')
 				i++;
@@ -121,12 +124,17 @@ int *get_max_val_arr(int **arr_pre, int num_str, int num_max_word)
 	int i;
 	int j;
 	int max_val;
-	int	*arr_res;
+	int	arr_res[num_max_word];
+	// char *arr_rez;
+	int *num;
 
 	i = 0;
 	j = 0;
 	max_val = 0;
-	arr_res = (int*)malloc(num_max_word * sizeof(int));
+
+	// arr_res = (int*)malloc(num_max_word);
+	// arr_rez = malloc(1);
+
 	while(i < num_max_word)
 	{
 		while(j < num_str)
@@ -140,24 +148,20 @@ int *get_max_val_arr(int **arr_pre, int num_str, int num_max_word)
 		j = 0;
 		i++;
 	}
-	i = num_str - 1;
-	while(i >= 0)
-		// free(arr_pre[i--]);
-	// free(arr_pre);
-	return arr_res;
+	num = arr_res;
+	return num;
 }
 
-void ft_column_output(int *arr_res,int num_column, char *str_file, int num_max_word)
+void ft_column_output(int *arr_res, char *str_file, int num_max_word)
 {
 	int	i;
 	int	j;
 	int n;
-	char *str;
+
 	int max_value;
 
 	i = 0;
 	j = 0;
-	n = 0;
 	max_value = 0;
 	while( i < num_max_word)
 	{
@@ -165,17 +169,19 @@ void ft_column_output(int *arr_res,int num_column, char *str_file, int num_max_w
 			max_value = arr_res[i];
 		i++;
 	}
+	n = 0;
+	// str = (char *)malloc(max_value + 1);
+	char str[max_value + 1];
+
 	i = 0;
-	str = (char *)malloc(max_value + 1);
-	if(str == NULL)
-		exit(1);
 	while(str_file[i] != '\0')
 	{
 		while (str_file[i] != SYMBOL && str_file[i] != '\n' && str_file[i] != '\0')
 			str[n++] = str_file[i++];
 		str[n] = '\0';
 		n = 0;
-		if (j < num_column)
+
+		if (j < NUM_COLUMN)
 		{
 			if (NEGATIVE)
 				printf("%-*s ", arr_res[j], str);
@@ -185,6 +191,7 @@ void ft_column_output(int *arr_res,int num_column, char *str_file, int num_max_w
 		else
 			printf("%s ", str);
 
+
 		if (j < num_max_word - 1)
 			j++;
 		else
@@ -193,12 +200,13 @@ void ft_column_output(int *arr_res,int num_column, char *str_file, int num_max_w
 			j = 0;
 		}
 		i++;
+
 	}
-	// free(arr_res);
-	// free(str);
+
+	// printf("", arr_res[j], )
 }
 
-int *get_max_val_field(char *str_file, int num_column)
+int *get_max_val_field(char *str_file)
 {
 	int i, j;
 	int **arr_pre;
@@ -206,54 +214,30 @@ int *get_max_val_field(char *str_file, int num_column)
 	int	num_str;
 	int	num_max_word;
 
-
 	i = 0;
+
 	num_str = get_num_str(str_file);
+
 	num_max_word = get_max_word(str_file);
+
+
 	arr_pre = get_arr_pre(str_file, num_str, num_max_word);
 	arr_res = get_max_val_arr(arr_pre, num_str, num_max_word);
-	ft_column_output(arr_res, num_column, str_file, num_max_word);
+	ft_column_output(arr_res, str_file, num_max_word);
 	return arr_res;
 }
 
-int main(int argc, char **argv)
+void main()
 {
-	int		i;
-	int		j;
 	int		fd;
 	int		*arr;
 	size_t	len;
 	char	*str_file;
-	int		num_column;
 
-	if (argc < 3)
-	{
-		printf("format: ./a.out [filename] -t [num_column]\n");
-		exit(1);
-	}
 
-	i = 0;
-	num_column = 0;
-	while  (i < argc)
-	{
-		if (!strcmp(argv[i], "-t"))
-		{
-			num_column = atoi(argv[i+1]);
-			break;
-		}
-		i++;
-	}
-	j = 0;
-	while(j < argc)
-	{
-		if (j != 0 && j != i && j != i + 1)
-			fd = open(argv[j], O_RDONLY);
-		j++;
-	}
-
+	fd = open(FILE, O_RDONLY);
 	len = get_len_file(fd);
 	str_file = file_to_str(fd, len);
-	arr = get_max_val_field(str_file, num_column);
-	// free(str_file);
-	close(fd);
+
+	arr = get_max_val_field(str_file);
 }
